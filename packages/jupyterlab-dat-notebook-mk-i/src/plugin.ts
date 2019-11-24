c0e5e6b76212a954e934d516decba8f3ecc3ea24
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-11-23 23:45:47 -0500

    start working on refactor

---
+++ packages/jupyterlab-dat-notebook-mk-i/src/plugin.ts
@@ -0,0 +1,31 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { MainAreaWidget } from '@jupyterlab/apputils';
+
+import { DatManager } from '@deathbeds/jupyterlab-dat/lib/manager';
+
+import { DatNotebookButton } from './datbutton';
+
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-dat-notebook-mki-i',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    const manager = new DatManager();
+    const { shell } = app;
+    const datButton = new DatNotebookButton(manager);
+
+    datButton.widgetRequested.connect((_it, content) => {
+      const main = new MainAreaWidget({ content });
+      shell.add(main, 'main', { mode: 'split-right' });
+    });
+
+    [datButton].forEach(button => {
+      app.docRegistry.addWidgetExtension('Notebook', button);
+    });
+  }
+};
+
+export default extension;
